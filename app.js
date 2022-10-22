c125ebe5ba62e891678c92731b5cdc05c4b9bf0c
raj10-04 <raj100shah1@gmail.com>
2022-10-22 18:52:54 +0530

    Main

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ app.post('/compose',function(req,res){
 })
 
 app.get('/posts/:postName',function(req,res){
-  const requestTitle = req.params.postName;
+  const requestTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
 
     if(storedTitle===requestTitle){
       console.log("Match!")
